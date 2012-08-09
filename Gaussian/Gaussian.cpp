8bf8a4de251e8e14410c59f5a5d438917533f69e
Clementine Ouziel <clementine.ouziel@crisalix.com>
2012-08-09 16:11:14 +0200

    EllipticBoundary function added

--- Gaussian/Gaussian.cpp
+++ Gaussian/Gaussian.cpp
@@ -1,126 +1,110 @@
 #include <iostream>
-#include "math.h"
 
 #include "cv.h"
 #include "highgui.h"
 
-cv::Mat Histogram(cv::Mat image,cv::Mat mask, int minRange, int maxRange)
-{
-
-   int histSize = maxRange-minRange;
-   float range[] = { minRange, maxRange } ;
-   const float* histRange = { range };
-   bool uniform = true;
-   bool accumulate = false;
-   cv::Mat hist;
-
-   cv::calcHist( &image, 1, 0, mask, hist, 1, &histSize, &histRange, uniform, accumulate );
-
-   return hist;
-
-}
+typedef cv::Matx<double, 1, 3> Matx13d;
+typedef cv::Matx<double, 3, 1> Matx31d;
+typedef cv::Matx<double, 3, 3> Matx33d;
 
 int main( int argc, char* argv[] )
 {
-  if( argc != 4 )
+    if( argc != 4)
+      {
+      std::cerr << "Usage: " << argv[0] << " <InputImage> <Mask> <colorspace> " << std::endl;
+      return EXIT_FAILURE;
+      }
+    cv::Mat image = cv::imread( argv[1] );
+    if(!image.data)
+      {
+      return EXIT_FAILURE;
+      }
+
+    cv::Mat mask = cv::imread( argv[2],0 );
+    if(!mask.data)
+      {
+      return EXIT_FAILURE;
+      }
+
+
+    if (strcmp(argv[3],"YCrCb")==0)
     {
-    std::cerr << "Usage: " << argv[0] << " <InputImage> <mask> <channel>" << std::endl;
-    return EXIT_FAILURE;
+       cv::cvtColor( image, image, CV_BGR2YCrCb );
     }
-  cv::Mat image = cv::imread( argv[1],1);
-  if(!image.data)
+    else if (strcmp(argv[3],"HSV")==0 )
     {
-    return EXIT_FAILURE;
+       cv::cvtColor( image, image, CV_BGR2HSV );
     }
-  cv::Mat mask = cv::imread( argv[2],0 );
-  if(!mask.data)
+    else if (strcmp(argv[3],"RGB")!=0)
     {
-    return EXIT_FAILURE;
+        std::cerr<<"Colorspaces: RGB or YCrCb or HSV"<<std::endl;
+        return EXIT_FAILURE;
     }
 
-  int minRange, maxRange;
-  cv::Mat im;
+    //Mu
+    Matx31d mu;
+    mu.zeros();
+    int den=0;
 
-  if (strcmp(argv[3],"B")==0 || strcmp(argv[3],"G")==0 || strcmp(argv[3],"R")==0)
-  {
-     maxRange=256;
-     minRange=0;
-     std::vector<cv::Mat> channels;
-     cv::split( image, channels );
-     if (strcmp(argv[3],"B")==0)
-         im=channels[0];
-     else if(strcmp(argv[3],"G")==0)
-         im=channels[1];
-     else
-         im=channels[2];
-  }
-  else if (strcmp(argv[3],"Y")==0 || strcmp(argv[3],"Cr")==0 || strcmp(argv[3],"Cb")==0)
-  {
-     minRange=16;
-     cv::Mat YCrCb;
-     cv::cvtColor( image, YCrCb, CV_BGR2YCrCb );
-     std::vector<cv::Mat> channels;
-     cv::split( YCrCb, channels );
-     if (strcmp(argv[3],"Y")==0)
-     {
-         im=channels[0];
-         maxRange=235;
-     }
-     else
-     {
-         maxRange=240;
-         if (strcmp(argv[3],"Cr")==0)
-             im=channels[1];
-         else
-             im=channels[2];
-     }
-  }
-  else if (strcmp(argv[3],"H")==0 || strcmp(argv[3],"S")==0 || strcmp(argv[3],"V")==0)
-  {
-     minRange=0;
-     cv::Mat HSV;
-     cv::cvtColor( image, HSV, CV_BGR2HSV );
-     std::vector<cv::Mat> channels;
-     cv::split( HSV, channels );
-     if (strcmp(argv[3],"H")==0)
-     {
-         im=channels[0];
-         maxRange=180;
-     }
-     else
-     {
-         maxRange=256;
-         if (strcmp(argv[3],"S")==0)
-             im=channels[1];
-         else
-             im=channels[2];
-     }
-  }
+    for (int i=0; i<image.rows;i++)
+        for (int j=0; j<image.cols; j++)
+        {
+            if(mask.at<float>(i,j)!=0)
+            {
+                den++;
+                mu(0)+=image.at<cv::Vec3b>(i,j)[0];
+                mu(1)+=image.at<cv::Vec3b>(i,j)[1];
+                mu(2)+=image.at<cv::Vec3b>(i,j)[2];
+            }
+        }
+    mu(0)/=den;
+    mu(1)/=den;
+    mu(2)/=den;
 
-  cv::Mat hist=Histogram(im,mask,minRange,maxRange);
+    //Sigma
+    Matx31d pixel;
+    Matx13d transPixel;
+    Matx33d sigma;
+    sigma.zeros();
+    Matx33d den1;
+    den1.all(1/(den-1));
 
-  float mu=0, sigma=0;
-  int den=0;
+    for (int i=0; i<image.rows;i++)
+        for (int j=0; j<image.cols; j++)
+        {
+            if(mask.at<float>(i,j)!=0)
+            {
+                pixel(0)=image.at<cv::Vec3b>(i,j)[0]-mu(0);
+                pixel(1)=image.at<cv::Vec3b>(i,j)[1]-mu(1);
+                pixel(2)=image.at<cv::Vec3b>(i,j)[2]-mu(2);
+                transPixel=pixel.t();
+                sigma+=pixel*transPixel;
+            }
+        }
 
-  //mu
-  for (int i=0;i<hist.rows;i++)
-  {
-      den+=hist.at<float>(i);
-      mu+=i*hist.at<float>(i);
-  }
-  mu/=den;
+    for(int i=0;i<3;i++)
+        for (int j=0;j<3;j++)
+        {
+            sigma (i,j)/=den-1;
+        }
 
-//sigma
-  for (int i=0;i<hist.rows;i++)
-  {
-      sigma+=(i-mu)*(i-mu)*hist.at<float>(i);
-  }
-  sigma/=(den-1);
+    //prob
+    std::vector<double> prob;
+    Matx33d invSigma;
+    double proba=0;
 
-  cv::Mat prob;
-  for (int i=0;i<hist.rows;i++)
-  {
-      prob.at<float>(i)=exp(-pow((i-mu),2)/(2*sigma))/(2*CV_PI*pow(fabs(sigma),1/2));
-  }
-  return 0;
+    for (int i=0; i<image.rows;i++)
+        for (int j=0; j<image.cols; j++)
+        {
+            pixel(0)=image.at<cv::Vec3b>(i,j)[0]-mu(0);
+            pixel(1)=image.at<cv::Vec3b>(i,j)[1]-mu(1);
+            pixel(2)=image.at<cv::Vec3b>(i,j)[2]-mu(2);
+            transPixel=pixel.t();
+            invSigma=sigma.inv();
+            proba=(transPixel*invSigma*pixel)(0);
+            prob.push_back(exp(-proba/2)/(2*CV_PI*pow(cv::determinant(sigma),1/2)));
+        }
+
+
+    return 0;
 }
